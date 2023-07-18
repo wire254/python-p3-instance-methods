49f0b6a2d2a32df95f7346193b4b48fcbf1c0551
Ulises <ulises.argueta95@gmail.com>
2023-07-18 14:53:34 -0500

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,13 @@
 
 class Dog:
     # Class body goes here
-
+    def bark(self):
+        print("Woof!")
     #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+    
+    
+ 
+    
+    
